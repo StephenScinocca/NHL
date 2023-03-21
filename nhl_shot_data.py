c7cccd7d3c55e35d9650bbcb127ecde539989d9d
StephenScinocca <StephenScinocca@gmail.com>
2023-03-21 10:12:46 -0700

    updated shot_data to include goalie team ID, and ran for 2021 2022 and 2020 2021 seasons

--- nhl_shot_data.py
+++ nhl_shot_data.py
@@ -29,12 +29,14 @@ def parse_command_line():
 
 def reset_shot_dict():
     shot_dict = {
+            "gameID": "NA",
             "eventTypeId": "NA",
             "shooterID": "NA",
             "shooterName": "NA",
             "shooterTeamID": "NA",
             "goalieID": "NA",
             "goalieName": "NA",
+            "goalieTeamID": "NA",
             "x": "NA",
             "y": "NA",
             "period": "NA",
@@ -42,14 +44,13 @@ def reset_shot_dict():
             "date": "NA",
             "emptyNet": "NA",
             "strength": "NA",
-            "gameID": "NA"
             }
     return shot_dict
 
-def set_shot_dict(shot_dict, shot, current_date, game_id):
-    shot_dict['eventTypeId'] = shot['result']['eventTypeId']
-    shot_dict['shooterTeamID'] = shot['team']['id']
+def set_shot_dict(shot_dict, shot, current_date, game_id, gameData):
     shot_dict['gameID'] = game_id
+    shot_dict['eventTypeId'] = shot['result']['eventTypeId']
+    shot_dict['shooterTeamID'] = shot['team']['id'] 
     try:
         shot_dict['x'] = shot['coordinates']['x']
         shot_dict['y'] = shot['coordinates']['y']
@@ -63,6 +64,12 @@ def set_shot_dict(shot_dict, shot, current_date, game_id):
         if p['playerType'] == "Goalie":
             shot_dict['goalieID'] = p['player']['id']
             shot_dict['goalieName'] = p['player']['fullName']
+   
+    if gameData['teams']['away'] != shot['team']['id']:
+        shot_dict['goalieTeamID'] = gameData['teams']['away']
+    else:
+        shot_dict['goalieTeamID'] = gameData['teams']['home']
+        
     shot_dict['period'] = shot['about']['period']
     shot_dict['periodTime'] = shot['about']['periodTime']
     shot_dict['date'] = current_date
@@ -101,7 +108,7 @@ elif seasonStart == 2020:
     schedule_link = 'https://statsapi.web.nhl.com/api/v1/schedule?startDate=12/01/{}&endDate=07/31/{}&expand=schedule.teams,schedule.linescore'.format(seasonStart, seasonEnd)
 schedule_data = download(schedule_link)
 
-header_shot = "eventTypeId,shooterID,shooterName,shooterTeamID,goalieID,goalieName,x,y,period,periodTime,date,emptyNet,strength,gameType"
+header_shot = "GameID,eventTypeId,shooterID,shooterName,shooterTeamID,goalieID,goalieName,goalieTeamID,x,y,period,periodTime,date,emptyNet,strength,gameType"
 counter = 0
 shot_d = reset_shot_dict()
 with open("data/shots_{}_{}.csv".format(seasonStart, seasonEnd), "w") as f:
@@ -115,15 +122,15 @@ with open("data/shots_{}_{}.csv".format(seasonStart, seasonEnd), "w") as f:
             link = 'https://statsapi.web.nhl.com/api/v1/game/{}/feed/live'.format(game_id)
             data = download(link)
             plays = data['liveData']['plays']['allPlays']
-            gameType = data['gameData']['game']['type']
-
+            gameData = data['gameData']
+            gameType = gameData['game']['type']
             for i in plays:
                 # counter +=1
                 if i['result']['eventTypeId'] == "SHOT" or i['result']['eventTypeId'] == "GOAL":
                     if i['about']['periodType'] != "SHOOTOUT":
                         # print(counter)
                         # counter +=1
-                        shot_d = set_shot_dict(shot_d, i, current_date, game_id)
+                        shot_d = set_shot_dict(shot_d, i, current_date, game_id, gameData)
                         for j in shot_d:
                             # print(shot_d[j])
                             f.write("{},".format(shot_d[j]))
